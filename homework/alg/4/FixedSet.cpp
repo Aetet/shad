d4f3ec51f1925fdda60dd1f8a18b1da297193351
abcdw <andrewtropin@gmail.com>
2013-11-17 13:11:46 +0400

    OK HashTable solution with simple hash function

--- homework/alg/4/FixedSet.cpp
+++ homework/alg/4/FixedSet.cpp
@@ -1,56 +1,49 @@
-#include <climits>
 #include <vector>
 #include <list>
 #include <iostream>
-#include <utility>
-#include <string>
 #include <algorithm>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::vector;
-using std::cerr;
 using std::list;
-using std::abs;
-using std::max;
-
-const int MAX_NUMBER_OF_ELEMENTS = 562879;
 
 class TFixedSet
 {
 public:
     TFixedSet()
-        : table(MAX_NUMBER_OF_ELEMENTS)
     {}
 
     void Initialize(const vector<int>& numbers)
     {
+        int numberOfElements = 1;
+        numbersDegree = 0;
+
+        while (numberOfElements <= numbers.size()) {
+            numberOfElements *= 2;
+            ++numbersDegree;
+        }
+
+        mod = sizeof(int) * 8 - numbersDegree;
+        table.resize(numberOfElements + 1);
+
         for (auto num : numbers) {
-            if (!Contains(num))
+            if (!Contains(num)) {
                 table[hash(num)].push_back(num);
+            }
         }
     }
 
-/*
-    static unsigned int hash(unsigned int number) {
-        number = ((number >> 16) ^ number) * 0x45d9f3b;
-        number = ((number >> 16) ^ number) * 0x45d9f3b;
+    unsigned int hash(unsigned int number) {
         number = ((number >> 16) ^ number);
-        return number >> 13;
+        number = ~number;
+        return number >> mod;
     }
-*/
-
-    int hash(int number)
-    {
-        // return MurmurHash2(&number, sizeof(number), 7) % MAX_NUMBER_OF_ELEMENTS;
-        return abs(number * 3) % MAX_NUMBER_OF_ELEMENTS;
-    }
-
 
     bool Contains(int number)
     {
-        int numHash = hash(number);
+        unsigned int numHash = hash(number);
         if (!table[numHash].empty()) {
             for (auto element : table[numHash]) {
                 if (element == number)
@@ -61,16 +54,10 @@ public:
         return false;
     }
 
-    void debugPrint()
-    {
-        int mx = 0;
-        for (auto elem : table)
-            mx = max(mx, static_cast<int>(elem.size()));
-        cerr << mx;
-    }
-
 private:
     vector<list<int> > table;
+    int numbersDegree;
+    int mod;
 };
 
 int main(int argc, char *argv[])
@@ -78,7 +65,9 @@ int main(int argc, char *argv[])
     std::ios_base::sync_with_stdio(false);
     int elementsNumber, requestsNumber;
     vector<int> initVector;
+
     cin >> elementsNumber;
+    initVector.reserve(elementsNumber);
 
     for (int i = 0; i < elementsNumber; ++i) {
         int element;
@@ -90,7 +79,6 @@ int main(int argc, char *argv[])
     st.Initialize(initVector);
 
     cin >> requestsNumber;
-
     for (int i = 0; i < requestsNumber; ++i) {
         int element;
         cin >> element;
@@ -99,3 +87,4 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
