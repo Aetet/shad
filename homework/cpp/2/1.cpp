d096bb8a1ebd0ff5547d01264908dbfbca5ba699
abcdw <andrewtropin@gmail.com>
2013-11-20 23:42:21 +0400

    Add Second homework cpp

---
+++ homework/cpp/2/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::getline;
+using std::cin;
+using std::cerr;
+using std::cout;
+
+int main(int argc, char *argv[])
+{
+    /* code */
+    return 0;
+}
